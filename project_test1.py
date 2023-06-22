7baa05be48629adf9d778cc5a5f9a51155922db2
ali <alirahimzade200444@gmail.com>
2023-06-23 01:08:32 +0430

    database created for login and sign up

--- project_test1.py
+++ project_test1.py
@@ -1,6 +1,11 @@
 from PyQt5 import QtWidgets , uic
 import sys
+import mysql.connector as mysql
+import sqlite3
+import pymysql
 from time import sleep
+e1 = sqlite3.connect('i.db')
+c = e1.cursor()
 class Myclass(QtWidgets.QMainWindow) :
     def __init__(self):
         super().__init__()
@@ -9,8 +14,50 @@ class Myclass(QtWidgets.QMainWindow) :
         self.sign_up.clicked.connect(self.sign_up1)
     def sign_up1(self):
         uic.loadUi('sign_up.ui', self)
+        self.sign_up2.clicked.connect(self.sign_up22)
+    def sign_up22(self):
+        c.execute("""
+            CREATE TABLE IF NOT EXISTS users (username text , password text);
+        """)
+        s = e1.execute("""
+            SELECT * FROM users;
+        """)
+        if (self.username_box.text() , self.password_box.text()) not in s.fetchall() :
+            # pass
+            c.execute("""
+                INSERT INTO users (username , password) VALUES (:username , :password)
+            """ , {'username' :self.username_box.text() , 'password' :self.password_box.text()})
+            s = e1.execute("""
+                SELECT * FROM users;
+            """)
+            print(s.fetchall())
+        # else :
+        #     print("another username with this password and username created")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# db = pymysql.connect(
+#     host = "localhost" ,
+#     user = "admin" ,
+#     password = "admin" ,
+# )
+# db = pymysql.connect(user='root', passwd='pwd', unix_socket="/tmp/mysql.sock")
+# print(db)
 
 
 app = QtWidgets.QApplication(sys.argv)
