235854b7f99be0deb21936ee64bfdc4e105c8665
ali <alirahimzade200444@gmail.com>
2023-06-26 23:14:05 +0430

    menu created completely

--- search branch/product.py
+++ search branch/product.py
@@ -116,6 +116,52 @@ def a() :
     s = 1
     for i in range(0 , len(products)) :
         products.pop(0)
+def b(word) :
+    global s
+    srcs = []
+    driver = webdriver.Chrome()
+    driver.get("https://basalam.com/s?q=" + str(word))
+    # driver.execute_script("window.scrollBy(0,6000)", "")
+    for i in range(1, 6):
+        driver.implicitly_wait(5)
+        name1 = driver.find_element(By.XPATH,
+                                    value='/html/body/div[1]/div/div/main/div/div[2]/div[1]/div[2]/div/div[2]/section/div[' + str(
+                                        i) + ']/div[2]/a')
+        driver.implicitly_wait(5)
+        price1 = driver.find_element(By.XPATH,
+                                     '/html/body/div[1]/div/div/main/div/div[2]/div[1]/div[2]/div/div[2]/section/div[' + str(
+                                         i) + ' ]/div[2]/div[3]/div[2]/span')
+        image = driver.find_element(By.XPATH,
+                                    value='/html/body/div[1]/div/div/main/div/div[2]/div[1]/div[2]/div/div[2]/section/div[' + str(
+                                        i) + ']/div[1]/a/img')
+        image_src = image.get_attribute("src")
+        srcs.append(image_src)
+        product1 = Product(str(s) + '.png', name1.text, price1.text)
+        products.append(product1)
+        print(product1)
+        s = s + 1
+    s = 1
+    for src in srcs:
+        response = requests.get(src)
+        open(str(s) + '.png', 'wb').write(response.content)
+        s = s + 1
+    s = 1
+    for src in srcs:
+        response = requests.get(src)
+        open(str(s) + '.png', 'wb').write(response.content)
+        s = s + 1
+    row = 1
+    col = 0
+    for product in products:
+        viewer = ImageViewer(product)
+        layout.addWidget(viewer.group_box, row, col)
+        col += 1
+        if col == 5:  # Change the number of columns as needed
+            col = 0
+            row += 1
+    s = 1
+    for i in range(0, len(products)):
+        products.pop(0)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
@@ -135,34 +181,61 @@ if __name__ == "__main__":
     # Add a search bar
     search_bar = QLineEdit()
     # Add the search bar at row 0, spanning 1 row and 5 columns
-    layout.addWidget(search_bar, 0, 1, 1, 1)
+    layout.addWidget(search_bar, 0, 1, 1 , 1 )
     layout2 = QGridLayout()
     layout.addLayout(layout2 , 0 , 0 , 1 , 1)
     search_btn = QtWidgets.QPushButton('search')
     layout.addWidget(search_btn, 0, 3, 1, 1)
     search_btn.clicked.connect(a)
-    phone_menu = QtWidgets.QMenu()
-    phone_menu.addAction('اپل')
-    phone_menu.addAction('سامسونگ')
-    phone_btn = QtWidgets.QPushButton('موبایل')
-    phone_btn.setMenu(phone_menu)
-    layout.addWidget(phone_btn , 0 , 4 , 1 , 1)
+    electronic_menu = QtWidgets.QMenu()
+
+    p1 = electronic_menu.addAction('یخچال و فریزر')
+    p2 = electronic_menu.addAction('ماشین لباسشویی')
+    p12 = electronic_menu.addAction('بخاری و گرمایشی')
+    p13 = electronic_menu.addAction('پنکه')
+
+    electronic_btn = QtWidgets.QPushButton('لوازم برقی')
+    electronic_btn.setMenu(electronic_menu)
+    layout.addWidget(electronic_btn , 0 , 4 , 1 , 1)
     food_menu = QtWidgets.QMenu()
-    food_menu.addAction('ماهی')
-    food_menu.addAction('برنج')
+    p3 = food_menu.addAction('ماهی')
+    p4 = food_menu.addAction('برنج')
+    p9 = food_menu.addAction('نان')
+    p10 = food_menu.addAction('شیرینی')
+    p11 = food_menu.addAction('فندق')
     food_btn = QtWidgets.QPushButton('خوراکی')
     food_btn.setMenu(food_menu)
     clothes_btn = QtWidgets.QPushButton('لباس')
     clothes_menu = QtWidgets.QMenu()
-    clothes_menu.addAction('کفش مردانه')
-    clothes_menu.addAction('شلوار مردانه')
-    clothes_menu.addAction('کفش زنانه')
-    clothes_menu.addAction('شلوار زنانه')
+    p5 = clothes_menu.addAction('کفش مردانه' )
+
+    p6 = clothes_menu.addAction('شلوار مردانه')
+    p7 = clothes_menu.addAction('کفش زنانه')
+    p8 = clothes_menu.addAction('شلوار زنانه')
     clothes_btn.setMenu(clothes_menu)
-    # clothes_btn.setGeometry(10 , 10 , 20 , 10)
-    layout2.addWidget(phone_btn, 0, 4, 1, 1)
+
+    layout2.addWidget(electronic_btn, 0, 4, 1, 1)
     layout2.addWidget(food_btn , 0 , 5 , 1 , 1)
     layout2.addWidget(clothes_btn , 0 , 6 , 1 , 1)
+
+
+    # l1 = [p1 , p2 , p3 , p4 , p5 , p6 , p7 , p8 , p9, p10 , p11]
+    p1.triggered.connect(lambda : b(p1.text()))
+    p2.triggered.connect(lambda : b(p2.text()))
+    p3.triggered.connect(lambda : b(p3.text()))
+    p4.triggered.connect(lambda : b(p4.text()))
+    p5.triggered.connect(lambda : b(p5.text()))
+    p6.triggered.connect(lambda : b(p6.text()))
+    p7.triggered.connect(lambda : b(p7.text()))
+    p8.triggered.connect(lambda : b(p8.text()))
+    p9.triggered.connect(lambda: b(p8.text()))
+    p10.triggered.connect(lambda: b(p8.text()))
+    p11.triggered.connect(lambda: b(p8.text()))
+    p12.triggered.connect(lambda: b(p8.text()))
+    p13.triggered.connect(lambda: b(p8.text()))
+    # for i in l1 :
+    #     print(i.text())
+    #     i.triggered.connect(lambda : b(i.text()))
 
 
 
